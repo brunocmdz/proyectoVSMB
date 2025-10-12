da05eeb520d0bf7a3fe978c14eebca75b303e595
unknown <brunorojass2212@gmail.com>
2025-10-12 16:45:54 -0300

    mejoras y a arreglos en el footer

--- VSMB/src/App.jsx
+++ VSMB/src/App.jsx
@@ -2,11 +2,15 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer'
 function App() {
   return (
-    <div className="container">
+    <div className="layout">
       <Navbar />
+      <main className="content">
+        {/* Tu contenido principal */}
+      </main>
       <Footer />
     </div>
   );
 }
+
 
 export default App;
--- VSMB/src/components/Footer.jsx
+++ VSMB/src/components/Footer.jsx
@@ -17,19 +17,11 @@ function Footer() {
                 </div>
             </div>
             <div className="footer-warning">
-                <p>EL JUEGO COMPULSIVO ES <strong>PERJUDICIAL</strong> PARA VOS Y TU FAMILIA.<br />COMUNICATE A LA LÍNEA
-                    DE ATENCIÓN
-                    <strong>GRATUITA 0800-666-6006.</strong>
+                <p>
+                    Pagina desarrollada en la Escuela Tecnica N°26 “Confederacion Suiza” <br/>
+                    Integrantes: Bruno Rojas - Shandee Incapuiño - Marcos Ortiz - Morales Victor
                 </p>
             </div>
-        </div>
-        <div className="footer-links">
-            <a href="#">Términos y condiciones</a> |
-            <a href="#">Reglas de juego</a> |
-            <a href="#">Política de cookies</a> |
-            <a href="#">Centro de ayuda</a> |
-            <a href="#">Juego responsable</a> |
-            <a href="#">Declaración de privacidad</a>
         </div>
     </footer>
     </div>
