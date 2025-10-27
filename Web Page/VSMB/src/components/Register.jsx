b76048abe0fdf65af75f8e0bfce76b5a670b0b2a
MarcosMartins1212 <marcosaliasrobertito@gmail.com>
2025-10-27 16:14:28 -0300

    implementar backend en frontend register

--- Web Page/VSMB/src/components/Register.jsx
+++ Web Page/VSMB/src/components/Register.jsx
@@ -1,6 +1,17 @@
 import './Register.css';
-
+import axios from 'axios';
+import { useState } from 'react';
 export default function Register() {
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState(''); 
+  const [name, setName] = useState(''); 
+  const [lastName, setLastName] = useState(''); 
+
+  const fetchRegister = async () => {
+    event.preventDefault();
+    const response = await axios.post(`http://localhost:3000/regist/?email=${email}&password=${password}&firstName=${name}&lastName=${lastName}`);
+    console.log(response.data);
+  }
   return (
     <div>
       {/* ===== Formulario ===== */}
@@ -9,14 +20,12 @@ export default function Register() {
 
         <form className="form-box">
           <div className="form-row">
-            <input type="text" placeholder="Nombre" />
-            <input type="text" placeholder="Apellido" />
+            <input type="text" placeholder="Nombre" onChange={(event) => setName(event.target.value)}/>
+            <input type="text" placeholder="Apellido" onChange={(event) => setLastName(event.target.value)}/>
           </div>
-          <input type="email" placeholder="Correo Electrónico" />
-          <input type="text" placeholder="Nombre de usuario" />
-          <input type="password" placeholder="Contraseña" />
-          <input type="password" placeholder="Confirme la contraseña" />
-          <button type="submit">Siguiente</button>
+          <input type="email" placeholder="Correo Electrónico" onChange={(event) => setEmail(event.target.value)}/>
+          <input type="password" placeholder="Contraseña" onChange={(event) => setPassword(event.target.value)}/>
+          <button onClick={fetchRegister} type="submit">Siguiente</button>
           <p className="form-terms">
             Creando una cuenta, acepta nuestra{' '}
             <a href="#">política de privacidad</a> y{' '}
